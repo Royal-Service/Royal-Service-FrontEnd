b466058861951d3ad6e3a957cd1b27e3a515d600
AbdallahMosa <abdallahmosa95@yahoo.com>
2023-01-18 23:54:49 +0200

    login

--- app/page.jsx
+++ app/page.jsx
@@ -7,7 +7,7 @@ import Container from "react-bootstrap/Container";
 import { AuthProvider } from "./contexts/Auth";
 import { useRouter,usePathname } from "next/navigation";
 // import { useAuth } from "./contexts/Auth";
-import Header from './components/Header'
+
 // const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
@@ -20,15 +20,9 @@ export default function Home() {
     <div>
      
      
-     <Header></Header>
+     
         <div style={{ overflow: "hidden", width: "100%" }}>
-        <button type="button" onClick={() => router.push('/login')}>
-                Login
-              </button>
-              <button type="button" onClick={() => router.push('/register')}>
-            
-                Register
-              </button>
+
 
           <Container className="mt-5 px-5">
             <Hero />
